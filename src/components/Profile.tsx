48320d9a3279dae74f434e75dcc44a1725ea969d
Vitor Cavalcante <v9cavalcante@gmail.com>
2021-03-03 11:52:27 -0300

    Dia 4

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,10 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/Profile.module.css';
 
 export function Profile(){
+    const {level} = useContext(ChallengesContext);
+
     return (
         <div className={styles.profileContainer}>
             <img src="https://pbs.twimg.com/profile_images/1355631596460765188/Ru6_jjVI_400x400.jpg" alt="Mio-chan"/>
@@ -8,7 +12,7 @@ export function Profile(){
                 <strong>Vitor Cavalcante</strong>
                 <p>
                     <img src="icons/level.svg" alt="level"/>
-                    Level 1
+                    Level {level}
                 </p>
             </div>
         </div>
